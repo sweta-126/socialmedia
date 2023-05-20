d6aeac89a440027a8d0232c60ee2e9b264c61c09
sweta-126 <78642144+sweta-126@users.noreply.github.com>
2023-05-20 22:34:46 +0530

    Update index.js

--- social-rest/index.js
+++ social-rest/index.js
@@ -4,12 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://socialmedia-app-mern-application.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
