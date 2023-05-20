aa15eaba05623300e04191a46e27f8928bb6390a
sweta-126 <78642144+sweta-126@users.noreply.github.com>
2023-05-20 22:31:06 +0530

    Update index.js

--- social-rest/index.js
+++ social-rest/index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://sunny-gingersnap-d7754a.netlify.app');
+  res.header('Access-Control-Allow-Origin', 'https://socialmedia-app-mern-application.netlify.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
